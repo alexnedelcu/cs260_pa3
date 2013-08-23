1e8152e680aa322c040355fb4de7db8cea85a18b
Samuel Kerr <sak328@cs.drexel.edu>
2013-08-22 22:44:40 -0400

    starting work on problem two

---
+++ p2/Dictionary.py
@@ -0,0 +1,11 @@
+class Dictionary:
+
+	hashtable=None
+	def __init__(self,B):
+		self.hashtable = {}
+		for i in xrange(0,B):
+			self.hashtable[i]=None
+
+	
+
+	
---
+++ p2/test.py
@@ -0,0 +1,6 @@
+from Dictionary import *
+
+dictionary=Dictionary(3)
+
+for item in dictionary.hashtable:
+	print item, dictionary.hashtable[item]
