11762ff335ad4a6683b7faa09c62a864bf18bda6
Samuel Kerr <sak328@cs.drexel.edu>
2013-08-24 16:56:36 -0400

    INSERT and DELETE methods

--- p2/Dictionary.py
+++ p2/Dictionary.py
@@ -9,4 +9,28 @@ class Dictionary:
 	def rehash(self, x):
 		return (x+1)%len(self.hashtable)
 
+	def INSERT(self, x):
+		bucket = 0
+		looped_through = False
+		while looped_through == False:
+			if self.hashtable[bucket] == x:
+				print "{} is already in Dictionary".format(x)
+				return
+			elif self.hashtable[bucket] == None:
+				self.hashtable[bucket] = x
+				return
+			if self.rehash(bucket) == 0:
+				looped_through=True
+				print "INSERT FAILED: Table is full"
+			bucket = self.rehash(bucket)
 
+	def DELETE(self, x):
+		bucket = 0
+		looped_through = False
+		while looped_through == False:
+			if self.hashtable[bucket] == x:
+				self.hashtable[bucket] = None
+			else:
+				if self.rehash(bucket) == 0:
+					looped_through = True
+				bucket = self.rehash(bucket)
--- p2/test.py
+++ p2/test.py
@@ -2,6 +2,14 @@ from Dictionary import *
 
 dictionary=Dictionary(3)
 
+
+dictionary.INSERT("A")
+dictionary.INSERT("A")
+dictionary.INSERT("Z")
+dictionary.INSERT("T")
+dictionary.INSERT("B")
 for item in dictionary.hashtable:
 	print item, dictionary.hashtable[item]
-	print dictionary.rehash(item)
+dictionary.DELETE("A")
+for item in dictionary.hashtable:
+	print item, dictionary.hashtable[item]
