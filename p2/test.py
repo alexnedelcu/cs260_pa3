37e3974457554c2995acd29d8f93903ad3ccb41b
Samuel Kerr <sak328@cs.drexel.edu>
2013-08-28 23:43:02 -0400

    added results portion of readme

--- p2/test.py
+++ p2/test.py
@@ -5,7 +5,9 @@ dictionary=Dictionary(10000)
 for x in xrange(0,5000):
 	dictionary.INSERT(x)
 
-dictionary.DELETE(5000)
+print "MEMBER(4999), before deleting:", dictionary.MEMBER(4999)
+dictionary.DELETE(4999)
+print "MEMBER(4999), after deleting:",dictionary.MEMBER(4999)
 
 print "MEMBER(298)", dictionary.MEMBER(298)
 print "MEMBER(4852)", dictionary.MEMBER(4852)
